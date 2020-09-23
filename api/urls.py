7d79641136e0388c856cc2a1d25d15d182a81331
tanchohang <tanchohang@gmail.com>
2020-09-23 11:09:33 +0100

    viewsets working with routers

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,16 @@
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
 
 from . import views
 
+router = DefaultRouter()
+router.register('posts', views.PostViewSet)
+
 urlpatterns = [
     path('', views.test),
-    path('posts', views.postlist)
+    path('posts-list', views.postlist),
+    path('post-create', views.postCreate),
+    path('', include(router.urls)),
+
 ]
--- api/views.py
+++ api/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import JsonResponse
 
 from rest_framework.decorators import api_view
+from rest_framework import viewsets
+
 from rest_framework.response import Response
 
 from .serializers import PostSerializer
@@ -21,3 +23,18 @@ def postlist(request):
     posts = Post.objects.all()
     serializers = PostSerializer(posts, many=True)
     return Response(serializers.data)
+
+
+@api_view(['POST'])
+def postCreate(request):
+    serializer = PostSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer

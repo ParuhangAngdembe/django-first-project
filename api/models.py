03d6aaecf2913f13b227f49fdc6b4c6f21d30748
tanchohang <tanchohang@gmail.com>
2020-09-26 11:34:47 +0100

    database reset

--- api/models.py
+++ api/models.py
@@ -1,15 +1,36 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 # Create your models here.
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=30)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
 
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    categories = models.ManyToManyField(Category)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-class Category(models.Model):
-    title=models.CharField(max_length=30)
-    description=models.TextField()
-
-class Tag(models.Model):
-    title=models.CharField(max_length=30)
+    def __str__(self):
+        return self.title
--- blog/models.py
+++ blog/models.py
@@ -3,18 +3,15 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
 
-class Post(models.Model):
-    title = models.CharField(max_length= 100)
-    content = models.TextField()
-    date_posted= models.DateTimeField(default= timezone.now)
-    author = models.ForeignKey(User, on_delete= models.CASCADE)
 
-    def __str__(self): #dunder
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    # creator = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):  # dunder
         return self.title
 
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk': self.pk})
-
-
-
-
